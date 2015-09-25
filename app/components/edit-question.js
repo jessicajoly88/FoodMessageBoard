7342abeb53b915171ca62e121018152c23a65733
Jessica <jessicajoly88@gmail.com>
2015-09-25 14:50:56 -0700

    Implement update question functionality to individual question page.

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+  	update(model) {
+  	  var params = {
+  	    question: this.get('question'),
+        author: this.get('author'),
+        notes: this.get('notes')
+  	  };
+  	  this.sendAction('update', model, params);
+  	}
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -3,6 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);	
+  }, 
+  actions: {
+  	update(model,params) {
+  	  Object.keys(params).forEach(function(key) {
+  	    if(params[key] !== undefined) {
+  	      model.set(key,params[key]);	
+  	    }	
+  	  });
+  	  model.save();
+  	  this.transitionTo('question');
+  	}
   }
 });  
   
